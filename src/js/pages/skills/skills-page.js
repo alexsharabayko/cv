1239570ebda41bc2384d1c635da636b240bc3ffb
Alex Sharabayko <alexsharabayko@gmail.com>
2016-06-01 11:37:41 +0300

    circle progress bar was added

--- src/js/pages/skills/skills-page.js
+++ src/js/pages/skills/skills-page.js
@@ -1,8 +1,24 @@
 var PageView = require('base/page-view');
+var CircleProgressBar = require('widgets/progress-bar/circle');
 
 var SkillsPage = PageView.extend({
     render: function () {
         this.$el.html(this.tmpl('skills/skills-page'));
+
+        var c = new CircleProgressBar({
+            color: 'green',
+            size: 400,
+            lineWidth: 10,
+            fontSize: '5rem',
+            fontColor: 'green'
+        });
+
+        document.body.innerHTML = '';
+        document.body.appendChild(c.el);
+
+        setTimeout(function () {
+            c.start();
+        }, 1000);
     }
 });
 
