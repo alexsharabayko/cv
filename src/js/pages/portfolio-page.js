536e0d88e86c70cb29b6e93372ff784050f99842
Alex Sharabayko <alexsharabayko@gmail.com>
2016-06-21 11:09:01 +0300

    Contact animations.

--- src/js/pages/portfolio-page.js
+++ src/js/pages/portfolio-page.js
@@ -1,8 +1,23 @@
 var PageView = require('base/page-view');
 
+var INTERVAL_DURATION = 300;
+
 var PortfolioPage = PageView.extend({
     url: 'data/portfolio.json',
-    template: 'pages/portfolio-page'
+    template: 'pages/portfolio-page',
+
+    handleSectionVisibleEvent: function (event) {
+        var $items = this.$('.fn-item');
+        var index = 0;
+
+        var interval = window.setInterval(function () {
+            $items.eq(index++).addClass('visible');
+
+            if (index === $items.length) {
+                window.clearInterval(interval);
+            }
+        }.bind(this), INTERVAL_DURATION)
+    }
 });
 
 module.exports = PortfolioPage;
