3c1324045ad8022ee2fb88d262f2b180833da7c7
Alex Sharabayko <alexsharabayko@gmail.com>
2016-06-22 10:08:42 +0300

    Fetch hotfix

--- src/js/init.js
+++ src/js/init.js
@@ -1,10 +1,12 @@
 //Tell to webpack to include css (less)
 require('less/common.less');
 
+//Fetch polifyl for old browsers
+require('fetch-polyfill');
+
 //Tell to webpack include images to build
 function requireAll(r) { r.keys().forEach(r); }
 requireAll(require.context('img/', true, /\.(ico|png|jpg)$/i));
-
 //Include layout elements (footer, header, etc.)
 require('layout/layout-manager');
 
